a4a6fd89fd1a23d5ef516d8b71badf708ec40e23
James He <62224382+he-is-james@users.noreply.github.com>
2021-05-15 16:38:38 -0700

    button on PostThumbnail to Page

--- babble-frontend/src/components/Feed/Feed.js
+++ babble-frontend/src/components/Feed/Feed.js
@@ -1,24 +1,29 @@
 import PostThumbnail from '../PostThumbnail/PostThumbnail'
 
-const tProfile = {
-    title: "What's up",
-    content: "Content, content, content, content, content, content, content, content",
-    likes: "2000",
+const tProfile1 = {
+    postId: 1,
+    title: "What's up1",
+    content: "Content1, content, content, content, content, content, content, content",
+    likes: "20",
+    comments: "10",
+}
+
+const tProfile2 = {
+    postId: 2,
+    title: "What's up2",
+    content: "Content2, content, content, content, content, content, content, content",
+    likes: "20000",
     comments: "10",
 }
 
 function Feed() {
     return (
-        <div className="Feed">
+        <div>
             <div className="Navigation">
 
             </div>
-            <div className="PostBody">
-                <PostThumbnail title={tProfile.title} content={tProfile.content} likes={tProfile.likes} comments={tProfile.comments}/>
-            </div>
-            <div className="Buttons">
-
-            </div>
+            <PostThumbnail {...tProfile1}/>
+            <PostThumbnail {...tProfile2}/>
         </div>
     )
 }
--- babble-frontend/src/components/PostThumbnail/PostThumbnail.js
+++ babble-frontend/src/components/PostThumbnail/PostThumbnail.js
@@ -1,6 +1,12 @@
 import './PostThumbnail.css'
+import { useHistory } from "react-router-dom";
 
 function PostThumbnail(props) {
+    const history = useHistory();
+    const goPost = (postId) => {
+        history.push(`/post/${postId}`);
+    }
+
     return (
         <div className="Thumbnail">
             <div className="Title">
@@ -11,6 +17,11 @@ function PostThumbnail(props) {
             </div>
             <div className="Numbers">
                 {props.likes} likes / {props.comments} comments
+            </div>
+            <div className="Button">
+                <button onClick = {() => goPost(props.postId)}>
+                    Post {props.postId}
+                </button>
             </div>
         </div>
     )
