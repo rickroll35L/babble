e0ca5bd18b9c4fa75f95ad8654141c24c04cc6d3
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-05-26 17:29:41 -0700

    Basic set up for axios requests, minus auth

--- babble-frontend/src/App.js
+++ babble-frontend/src/App.js
@@ -1,13 +1,136 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Home from "./pages/Home/Home.js";
 import Login from "./pages/Login/Login.js";
 import Post from "./pages/Post/Post.js";
 import Profile from "./pages/Profile/Profile.js";
+import axios from 'axios';
 
+// localStorage.setItem(
+//   "AuthToken",
+//   "Bearer FAKETOKEN#ASDOANSCOAOCBDIVB",
+// );
+const token = localStorage.getItem("AuthToken");
+const apiURL = "http://localhost:8080/";
+
+if (token) {
+
+}
+
+const authAxios = axios.create({
+  baseURL: apiURL,
+  headers: {
+    Authorization: token
+  }
+});
 
 function App() {
+
+  const getUser = useCallback(async (uid) => {
+    try {
+      const result = await authAxios.get(`/users/${uid}`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const createUser = useCallback(async (body) => {
+    try {
+      const result = await authAxios.post(`/users/create-user`, body);
+      if(result==="User was added"){
+        return getUser(body.id) // need to put auth somewhere too
+      }
+      else {
+        return null;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const deleteUser = useCallback(async (uid) => {
+    try {
+      const result = await authAxios.delete(`/users/${uid}/delete`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const getPosts = useCallback(async () => {
+    try {
+      const result = await authAxios.get(`/posts`);
+      setPosts(result);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const getPost = useCallback(async (pid) => {
+    try {
+      const result = await authAxios.get(`/posts/${pid}`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const createPost = useCallback(async (uid) => {
+    try {
+      const result = await authAxios.post(`/users/${uid}/create-post`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const deletePost = useCallback(async (pid) => {
+    try {
+      const result = await authAxios.delete(`/posts/${pid}/delete`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const makeComment = useCallback(async (props) => {
+    try {
+      const result = await authAxios.post(`/users/${props.uid}/comment/${props.pid}`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const likePost = useCallback(async (props) => {
+    try {
+      const result = await authAxios.post(`/users/${props.uid}/like/${props.pid}`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const savePost = useCallback(async (props) => {
+    try {
+      const result = await authAxios.post(`/users/${props.uid}/save/${props.pid}`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  const searchPost = useCallback(async (query) => {
+    try {
+      const result = await authAxios.get(`/posts/search/${query}`);
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   return (
     <div className="App">
       <Router>
