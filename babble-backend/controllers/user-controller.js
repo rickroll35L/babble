c18587cd7294ad4a645e172a10c3bf52b3382a87
RupinMittal <42721308+RupinMittal@users.noreply.github.com>
2021-05-29 01:41:08 -0700

    Add routes for getting user posts
    App should be able to retrieve user's saved posts and the
    posts the user made

--- babble-backend/controllers/user-controller.js
+++ babble-backend/controllers/user-controller.js
@@ -3,8 +3,14 @@ const db = require('../database/db');
 module.exports = {
     changePassword,
     changeEmail,
-    deleteAccount
+    deleteAccount,
+    getSavedPosts,
+    getMyPosts
 }
+
+/*************************************** */
+/* Get the userid with res.locals.userid */
+/*************************************** */
 
 /* change user password, req.body: email, oldPassword, newPassword */
 async function changePassword (req, res) {
@@ -22,4 +28,16 @@ async function changeEmail (req, res) {
 async function deleteAccount (req, res) {
     // TODO
     res.send('delete account');
+}
+
+/* get saved posts */
+async function getSavedPosts (req, res) {
+    // TODO
+    res.send('got saved posts');
+}
+
+/* get posts the user made */
+async function getMyPosts (req, res) {
+    // TODO
+    res.send('got my posts');
 }
--- babble-backend/routes/user.js
+++ babble-backend/routes/user.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const user_router = express.Router();
-const { isAuth } = require('../authentication/auth');
+const { isAuth, logout } = require('../authentication/auth');
 const { auth_errorHandler } = require('../authentication/auth-errorhandler');
 const {
     changePassword,
     changeEmail,
-    deleteAccount
+    deleteAccount,
+    getSavedPosts,
+    getMyPosts
 } = require('../controllers/user-controller');
 
 module.exports = user_router;
 
-/* All requests related to the user will requrie autentication */
+/* All requests related to the user will require autentication 
+   token in the header */
 user_router.use(isAuth, auth_errorHandler);
 
 /************************************** routes **************************************/
@@ -24,3 +27,11 @@ user_router.post('/change-email', changeEmail);
 /* delete account, req.body: email, password */
 user_router.delete('/delete-account', deleteAccount);
 
+/* get saved posts */
+user_router.get('/saved-posts', getSavedPosts);
+
+/* get posts the user made */
+user_router.get('/my-posts', getMyPosts);
+
+/* log out */
+user_router.post('/logout', logout, (req, res) => {});
