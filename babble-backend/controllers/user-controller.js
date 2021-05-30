4fefd1a8afb79799914d268a93d8ea6b1fade6cf
Rupin Mittal <42721308+RupinMittal@users.noreply.github.com>
2021-05-29 18:20:08 -0700

    Refactor routes (#20)
    * Refactor authentication to write to database in middlewares
    
    It's a cleaner implementation to handle writing to the
    database in the encapsulated middlewares. This is also a step
    towards using express-router and controllers
    
    * Rename auth_errorHandler
    
    * Add enter router for login/signup
    
    For better modularity, routes using similar paths
    and middlewares should be refactored to use express
    routers and controllers. This the first step
    
    * Add routes to change user settings
    
    User should be able to change their email, password,
    and delete their account
    
    * Change database references from 'local' to 'db'
    
    * Add routes for getting user posts
    
    App should be able to retrieve user's saved posts and the
    posts the user made
    
    * Add remaining post routes and small implementation changes
    
    Add router/controller for posts routes. Changed database to
    load data automatically when it's required with 'require'.
    Left suggestions on delete-post and get-post.
    
    * Remove uneccessary 'async' users in controllers
    
    * Remove uneeded routes.js file
    
    All routes have been written in routers and controllers
    
    * Refactor index.js
    
    Remove now uneeded code and rename to server.js
    
    * Implemented all routes except change password, change email, delete account, and search.
    
     * Database data is now only loaded once on server start.
     * added testing framework for authorized requests
     * altered delete post endpoint so that clients can only delete posts that they created
     * Fixed doc errors
    
    * Fixed leftover query -> req.params
    
    Co-authored-by: EinarBalan <einar.balan@gmail.com>
    Co-authored-by: Einar Balan <40647794+EinarBalan@users.noreply.github.com>

---
+++ babble-backend/controllers/user-controller.js
@@ -0,0 +1,72 @@
+const db = require('../database/db');
+const shared = require('./shared');
+
+module.exports = {
+    changePassword,
+    changeEmail,
+    deleteAccount,
+    getSavedPosts,
+    getMyPosts,
+    deletePost
+}
+
+/*************************************** */
+/* Get the userid with res.locals.userid */
+/*************************************** */
+
+/* change user password, req.body: email, oldPassword, newPassword */
+function changePassword(req, res) {
+    // TODO
+    res.send('change password');
+}
+
+/* change user email, req.body: oldEmail, newEmail, password */
+function changeEmail(req, res) {
+    // TODO
+    res.send('change email');
+}
+
+/* delete account, req.body: email, password */
+function deleteAccount(req, res) {
+    // TODO
+    res.send('delete account');
+}
+
+/* helper function: get all nondeleted posts as array of objects */
+function getPosts(res, posts) {
+    const user = db.users[res.locals.userid];
+    const feed = db.posts.feed;
+    const filtered = user[posts].reduce((result, id) => {
+        if (!feed[id].isDeleted)
+            result.push(feed[id]);
+            return result;
+    }, []);
+    return filtered;
+}
+
+/* get saved posts */
+function getSavedPosts(_req, res) {
+    const saved = getPosts(res, "saved");
+    res.status(200).json(saved);
+}
+
+/* get posts the user made */
+function getMyPosts(_req, res) {
+    const posts = getPosts(res, "posts");
+    res.status(200).json(posts);
+}
+
+/* delete a post made the user, req.params: post id (pid) */
+function deletePost(req, res) {
+    shared.verifyPID(req, res, () => {
+        const post = db.posts.feed[req.params.pid];
+        if (post.poster === res.locals.userid) {
+            post.isDeleted = true;
+            db.writePosts();
+            res.send(`Post ${req.params.pid} was deleted`);
+        }
+        else {
+            res.status(403).send("User cannot delete posts they did not create.")
+        }
+    });
+}
