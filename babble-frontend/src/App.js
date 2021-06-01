6a6113701f8965fec4be37b68ab2061c39005126
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2021-06-01 15:33:06 -0700

    hotfix to a few breaking bugs

--- babble-frontend/src/App.js
+++ babble-frontend/src/App.js
@@ -298,6 +298,7 @@ function App() {
                 getPost={getPost} 
                 savePost={savePost} 
                 likePost={likePost}
+                createPost={createPost}
               />
             )}
           />
@@ -313,6 +314,7 @@ function App() {
                 getSavedPosts={getSavedPosts}
                 getMyPosts={getMyPosts}
                 logoutUser={logoutUser}
+                createPost={createPost}
               />
             )}
           />
